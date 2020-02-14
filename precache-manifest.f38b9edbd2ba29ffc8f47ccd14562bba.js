cd0a14e0359ea6a9fe4f71d47656ce82687b56fd
Jean Tubay <jean.tubay.92@gmail.com>
2020-02-14 13:41:17 -0500

    Updates

---
+++ precache-manifest.f38b9edbd2ba29ffc8f47ccd14562bba.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4588d1828be153928f42d0420d94be71",
+    "url": "/ReactPetMatch/index.html"
+  },
+  {
+    "revision": "293080386dde09c39fa9",
+    "url": "/ReactPetMatch/static/css/main.97f1e7cb.chunk.css"
+  },
+  {
+    "revision": "93a3bf72f2486a9f482e",
+    "url": "/ReactPetMatch/static/js/2.43af81c8.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/ReactPetMatch/static/js/2.43af81c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "293080386dde09c39fa9",
+    "url": "/ReactPetMatch/static/js/main.1cc15a69.chunk.js"
+  },
+  {
+    "revision": "4cd98ace59f9176e7171",
+    "url": "/ReactPetMatch/static/js/runtime-main.742fdbf8.js"
+  }
+]);
